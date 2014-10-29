f97276976830cfae5a6cfd4a6a920c10c524eaf8
Igor Kur <kur91@mail.ru>
2014-10-30 00:30:26 +0200

    some improvments in tests

---
+++ test/helpers/stub-graph.js
@@ -0,0 +1,28 @@
+"use strict";
+define(
+  ['../../lib/metaelement'],
+  function(MetaElement) {
+    return function stubGraph(){
+      var els = [],
+          els1 = [],
+          els2 = [];
+
+      for (var i = 0; i < 5; i++) {
+        els.push(new MetaElement('a' + i.toString()));
+      }
+
+      for (var i = 0; i < 3; i++) {
+        els1.push(new MetaElement('b' + i.toString()));
+        els1[i].addParent(els[Math.ceil(Math.random() * 4)]);
+      }
+
+      for (var i = 0; i < 2; i++) {
+        els2.push(new MetaElement('c' + i.toString()));
+        els2[i].addParent(els1[Math.ceil(Math.random() * 2)]);
+      }
+      var rs = els1.concat(els).concat(els2);
+
+      return rs;
+    }
+  }
+);
--- test/metaelement-test.js
+++ test/metaelement-test.js
@@ -1,35 +1,25 @@
 "use strict";
 define(
-    ['../lib/metaelement', '../lib/metagraph'],
-    function(metaelement, metagraph) {
-      function stubGraph(){
-        var els = [],
-            els1 = [],
-            els2 = [];
-
-        for (var i = 0; i < 5; i++) {
-          els.push(new metaelement(0,0,'a' + i.toString()));
-        }
-
-        for (var i = 0; i < 3; i++) {
-          els1.push(new metaelement(0,0,'b' + i.toString()));
-          els1[i].addParent(els[Math.ceil(Math.random() * 4)]);
-        }
-
-        for (var i = 0; i < 2; i++) {
-          els2.push(new metaelement(0,0,'c' + i.toString()));
-          els2[i].addParent(els1[Math.ceil(Math.random() * 2)]);
-        }
-        var rs = els1.concat(els).concat(els2);
-
-        return rs;
-      }
+    ['../lib/metaelement', '../lib/metagraph', 'helpers/stub-graph'],
+    function(metaelement, Metagraph, stubGraph) {
       var run = function() {
-          test('levels', function(){
+          test('.levels', function(){
             expect(3);
 
-            var division = metagraph.levels(stubGraph());
+            var division = Metagraph.levels(stubGraph());
 
+            equal(division['level0'].length, 5);
+            equal(division['level1'].length, 3);
+            equal(division['level2'].length, 2);
+          });
+
+          test('#levels', function(){
+            expect(3);
+
+            var meta = Metagraph();
+            meta.addVertices(stubGraph());
+
+            var division = meta.levels();
             equal(division['level0'].length, 5);
             equal(division['level1'].length, 3);
             equal(division['level2'].length, 2);
