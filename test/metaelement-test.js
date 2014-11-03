06ecfabb776c76da0d2472cd34e112f72e9fd236
Igor Kur <kur91@mail.ru>
2014-11-04 00:46:23 +0200

    add levels for metaelement

--- test/metaelement-test.js
+++ test/metaelement-test.js
@@ -5,15 +5,31 @@ define(
       var run = function() {
         module('MetaElement');
 
-        test('#findRoot', function(){
+        test('#findRoots', function(){
           expect(2);
 
           var meta = new MetaElement('a');
           var meta1 = new MetaElement('b');
           meta.addParentInner(meta1);
 
-          deepEqual(meta.findRoot(), meta1);
-          deepEqual(meta.findRoot(), meta1);
+          deepEqual(meta.findRoots(), [meta1]);
+          deepEqual(meta1.findRoots(), [meta1]);
+        });
+
+        test('#levels', function(){
+          expect(4);
+
+          var meta = new MetaElement('a');
+          var meta1 = new MetaElement('b');
+          meta.addParentInner(meta1);
+
+          var division = meta.levels();
+          deepEqual(division['level0'], [meta1]);
+          deepEqual(division['level1'], [meta]);
+
+          var division1 = meta1.levels();
+          deepEqual(division1['level0'], [meta1]);
+          deepEqual(division1['level1'], [meta]);
         });
       };
       return {run: run}
