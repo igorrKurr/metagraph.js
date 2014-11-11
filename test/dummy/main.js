d7a1c08e864dbf659fb4da83ff0ef20cf41e92fb
Igor Kur <kur91@mail.ru>
2014-11-11 23:08:09 +0200

    add edges

--- test/dummy/main.js
+++ test/dummy/main.js
@@ -8,16 +8,19 @@ require(
 
         for (var i = 0; i < 5; i++) {
           els.push(Metagraph.MetaElement('a' + i.toString()));
+          els[i].appendTo(m);
         }
 
         for (var i = 0; i < 3; i++) {
           els1.push(Metagraph.MetaElement('b' + i.toString()));
           els1[i].addParent(els[Math.ceil(Math.random() * 4)]);
+          els1[i].appendTo(m);
         }
 
         for (var i = 0; i < 2; i++) {
           els2.push(Metagraph.MetaElement('c' + i.toString()));
           els2[i].addParent(els1[Math.ceil(Math.random() * 2)]);
+          els2[i].appendTo(m);
         }
         var rs = els1.concat(els).concat(els2);
 
